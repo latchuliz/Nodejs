a04eee5dd0823fe1cd99cc66481a134ff5916f7a
latchuliz <45194018+latchuliz@users.noreply.github.com>
2020-01-29 19:09:22 +0530

    addedtest

---
+++ math.js
@@ -0,0 +1,7 @@
+class Add {
+  add(a, b) {
+    return a + b;
+  }
+}
+
+module.exports = Add;
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var expect = require('expect.js');
+const Add = require('../math.js');
+const add = new Add();
+describe('test suite', function () {
+    it('should do math', function () {
+    expect(add.add(1, 3)).to.equal(4);
+  });
+});
