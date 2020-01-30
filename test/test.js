c6ea9e38c53097a0be843f1faf29c428c2c2ea1a
latchuliz <45194018+latchuliz@users.noreply.github.com>
2020-01-30 13:30:37 +0530

    u

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const Add = require('../math.js');
 const add = new Add();
 describe('test suite', function () {
     it('should do math', function () {
-    expect(add.add(1, 3)).to.equal(5);
+    expect(add.add(1, 3)).to.equal(4);
   });
 });
