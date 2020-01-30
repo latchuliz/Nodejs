910b5c7e8de4a809d83425a4dae76d12da8eb73a
latchuliz <45194018+latchuliz@users.noreply.github.com>
2020-01-30 11:08:00 +0530

    u

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello GoGoAir- Welcome to Devops Project\n');
+  res.end('Hello GoGoAir- Welcome to Devops project\n');
 });
 
 server.listen(port, hostname, () => {
